931a84ad46b8da05c24bbaecda91772916d76de8
Naoki Hirayama <bfmv.na.metalmaster820@gmail.com>
2021-10-03 16:09:12 +0900

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfullMessage from "./components/ColorfullMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
 
   return (
     <>
       <h1 style={{ color: "red" }}>hello</h1>
       <ColorfullMessage color="blue">お元気ですか？</ColorfullMessage>
       <ColorfullMessage color="pink">お元気です</ColorfullMessage>
-      <button onClick={onClickButton}>buttun</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
